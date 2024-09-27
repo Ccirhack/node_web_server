08b2973984254ff66608bcdaee25c4cb169024f3
Ccirhack <yuan.retamozo.27@unsch.edu.pe>
2024-09-27 00:44:05 -0500

    Realizamos un servidor web donde servimos una red social como la del pajarito

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const { envs } = require("./config/env")
+const { startServer } = require("./server/server")
+
+
+const main = () => {
+    startServer({
+        port: envs.PORT,
+        public_path: envs.PUBLIC_PATH
+    })
+}
+
+// Función agnóstica auto convocada
+// Agnóstica por que no tiene nombre
+// Autoconvocada porque la ejecutamos con los paréntesis
+( async () => {
+    main()
+})()
